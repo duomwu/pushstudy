8b3d50b13f0c3465261fd8f9a3cc6acd12a79cd2
allenwm <506763070@qq.com>
2017-03-09 13:53:37 +0800

    test push team : A

--- main.cpp
+++ main.cpp
@@ -7,5 +7,6 @@ int main(int argv, char argc[])
 	cout << "hello world" << endl;
 	cout << "allen" << endl;
 	cout << "add main team : B" << endl;
+	cout << "test push team : A" << endl;
 	return 0;
 }
